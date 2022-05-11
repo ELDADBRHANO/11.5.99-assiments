2d98927d9fcb36888e1813da82e58190ad491ea1
ELDADBRHANO <ebrhano@gmail.com>
2022-05-11 21:18:08 +0300

    first_commit

---
+++ main.js
@@ -0,0 +1,125 @@
+// function Hello(){
+//   document.write("hello world")
+// }
+// var i = 0
+// while (i<3){
+//   Hello()
+//   i++
+// }
+
+// function max(num1,num2){
+
+//   var num1=+prompt("enter number")
+//   var num2=+prompt("enter number")
+//   if(num1>num2){
+//     return console.log(num1)
+//   }
+//   else{
+//     return console.log(num2)
+//   }
+// }
+// max()
+
+
+
+// function sum(num1,num2) {
+// var num1 = +prompt()
+// var num2 = +prompt()
+// if(num1>=num2){
+//   return num1+num2
+// }
+// else{
+//   return num1+num2
+//   }
+// }
+
+// console.log(sum())
+
+
+
+// var x = 0  
+// var y = 2
+// function myFunction()
+// {
+//   var j = x;
+//   x = y; 
+//   y=j;
+// }
+// document.write(x , y); 
+// myFunction();
+// document.write(x , y); 
+
+
+
+// function myNum() {
+
+
+// var i =0
+// while(i<5){
+//   console.log("hello world")
+//   i++
+// }
+// }
+// myNum()
+
+
+// function min(num1,num2) {
+  
+
+// var num1 =+prompt()
+// var num2 =+prompt()
+// if(num1<num2){
+//   console.log(num1)
+// }
+// else{
+//   (num1>num2)
+//   console.log(num2)
+//   }
+// }
+// min()
+
+
+// var num1 = +prompt()
+// var num2 = +prompt()
+// var num3 = +prompt()
+
+// if(num1<num2){
+//   console.log(num1);
+// }
+// else if(num2<num1&&num3){
+//   console.log(num2);
+// }
+// else (num3<num2&&num1)
+// console.log(num3);
+
+// else if (num3<num1&&num2){
+//    console.log(num3);
+// }
+ 
+
+
+// Java implementation to find
+// the smallest of three elements
+
+
+
+
+
+
+
+	// var a=+prompt()
+  // var b=+prompt()
+  // var c=+prompt()
+
+
+	// if (a <= b && a <= c)
+	// 	console.log( a + " is the smallest");
+
+	// else if (b <= a && b <= c)
+	// console.log( b + " is the smallest");
+
+	// else
+  // console.log( c + " is the smallest");
+
+	
+
